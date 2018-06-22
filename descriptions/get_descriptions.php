3118052220270e6ee6fc0d90b3ff4531069b89df
Matěj Suchánek <matejsuchanek97@gmail.com>
2018-06-22 17:56:24 +0200

    commit files

---
+++ descriptions/get_descriptions.php
@@ -0,0 +1,127 @@
+<?php
+
+header( 'Content-type: application/json' );
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+
+$action = isset( $_GET['action'] ) ? $_GET['action'] : '';
+$callback = isset( $_GET['callback'] ) ? $_GET['callback'] : '';
+
+$ts_pw = posix_getpwuid( posix_getuid() );
+$ts_mycnf = parse_ini_file( $ts_pw['dir'] . '/replica.my.cnf' );
+$db = mysql_connect( 'tools.db.svc.eqiad.wmflabs', $ts_mycnf['user'], $ts_mycnf['password'] );
+$wd = mysql_connect( 'wikidatawiki.analytics.db.svc.eqiad.wmflabs', $ts_mycnf['user'], $ts_mycnf['password'] );
+
+mysql_select_db( $ts_mycnf['user'] . '__data', $db );
+mysql_select_db( 'wikidatawiki_p', $wd );
+
+unset( $ts_mycnf, $ts_pw );
+
+$data = [];
+
+if ( $action == 'desc' ) {
+
+	$data = [
+		'label' => [ 'en' => 'Items without descriptions' ] ,
+		'description' => [ 'en' => '' ],
+		'icon' => '',
+	];
+
+} elseif ( $action == 'tiles' ) {
+
+	$count = isset( $_GET['num'] ) ? intval( $_GET['num'] ) : 1;
+	$lang = isset( $_GET['lang'] ) ? $_GET['lang'] : '';
+	$lang = $lang === 'cs' ? $lang : 'en';
+	$data['tiles'] = [];
+	while ( count( $data['tiles'] ) < $count ) {
+		$random = rand( 0, pow( 2, 32 ) - 1 );
+		$query  = "SELECT id, item, description FROM descriptions";
+		$query .= " WHERE random >= $random AND lang = '$lang' AND status IS NULL";
+		$query .= " ORDER BY random LIMIT " . ($count*2);
+		$result = mysql_query( $query, $db );
+		if ( !$result ) {
+			continue;
+		}
+		while ( $row = mysql_fetch_object( $result ) ) {
+			$item = $row->item;
+			$check_result = mysql_query(
+				"SELECT term_text FROM wb_terms WHERE term_full_entity_id = '$item'" .
+				" AND term_language = '$lang' AND term_type = 'description'", $wd );
+			if ( mysql_fetch_object( $check_result ) ) {
+				mysql_query(
+					"UPDATE descriptions SET status = 'REPLACED'" .
+					" WHERE item = '$item' AND lang = '$lang'", $db );
+				continue;
+			}
+			$check_result = mysql_query( "SELECT page_is_redirect FROM page" .
+				" WHERE page_namespace = 0 AND page_title = '$item'", $wd );
+			$item_row = mysql_fetch_object( $check_result );
+			if ( !$item_row ) {
+				mysql_query( "UPDATE descriptions SET status = 'DELETED' WHERE item = '$item'", $db );
+				continue;
+			}
+			if ( $item_row->page_is_redirect == '1' ) {
+				mysql_query( "UPDATE descriptions SET status = 'DELETED' WHERE item = '$item'", $db );
+				continue;
+			}
+			$tile = [];
+			$tile['id'] = $row->id;
+			$tile['sections'] = [
+				[ 'type' => 'item', 'q' => $row->item ],
+				[ 'type' => 'text', 'title' => 'Is this a good description?', 'text' => $row->description ],
+			];
+			$tile['controls'] = [
+				[
+					'type' => 'buttons',
+					'entries' => [
+						[
+							'type' => 'green',
+							'decision' => 'yes',
+							'label' => 'Yes',
+							'api_action' => [
+								'action' => 'wbsetdescription',
+								'id' => $row->item,
+								'language' => $lang,
+								'value' => $row->description,
+							],
+						],
+						[ 'type' => 'white', 'decision' => 'skip', 'label' => "Don't know" ],
+						[ 'type' => 'red', 'decision' => 'no', 'label' => 'No' ],
+					],
+				],
+			];
+			$data['tiles'][] = $tile;
+			if ( count( $data['tiles'] ) === $count ) {
+				break;
+			}
+		}
+	}
+
+} elseif ( $action == 'log_action' ) {
+
+	$tile = isset( $_GET['tile'] ) ? $_GET['tile'] : '';
+	$decision = isset( $_GET['decision'] ) ? $_GET['decision'] : '';
+	if ( $decision === 'yes' ) {
+		$query = "SELECT lang, item FROM descriptions WHERE id = '$tile'";
+		$row = mysql_fetch_object( mysql_query( $query, $db ) );
+		$query = "UPDATE descriptions SET status = 'DONE' WHERE id = '$tile'";
+		mysql_query( $query, $db );
+		$query = "UPDATE descriptions SET status = 'REPLACED'";
+		$query .= " WHERE lang = '{$row->lang}' AND item = '{$row->item}'";
+		$query .= " AND status IS NULL";
+		mysql_query( $query, $db );
+	} elseif ( $decision === 'no' ) {
+		$query = "UPDATE descriptions SET status = 'NO' WHERE id = '$tile'";
+		mysql_query( $query, $db );
+	}
+
+} else {
+	
+}
+
+mysql_close( $db );
+mysql_close( $wd );
+
+echo "{$callback}(";
+echo json_encode( $data );
+echo ")\n";
---
+++ descriptions/harvest_descriptions.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+import pymysql
+import pywikibot
+import random
+import re
+
+from pywikibot import link_regex as LINK_REGEX
+from pywikibot.bot import CurrentPageBot, SingleSiteBot
+from pywikibot.pagegenerators import (
+    GeneratorFactory,
+    PreloadingEntityGenerator,
+)
+from pywikibot.textlib import (
+    FILE_LINK_REGEX as frpattern,
+    NESTED_TEMPLATE_REGEX,
+)
+
+
+class DescriptionsBot(CurrentPageBot, SingleSiteBot):
+
+    def __init__(self, db, **kwargs):
+        self.availableOptions.update({
+            'min_words': 2,
+        })
+        super(DescriptionsBot, self).__init__(**kwargs)
+        self.db = db
+        self.COMMENT_REGEX = re.compile('<!--.*?-->') # todo: from textlib
+        self.FILE_LINK_REGEX = re.compile(
+            frpattern % '|'.join(self.site.namespaces[6]))
+        self.FORMATTING_REGEX = re.compile("('{5}|'{2,3})")
+        self.REF_REGEX = re.compile(r'<ref.*?(>.*?</ref|/)>')
+        self.regex = self.get_regex_for_title(r'[^\|\]]+')
+
+    def get_regex_for_title(self, escaped_title):
+        pattern = r'^\*+ *\[\[(%s)(?:\|[^][]+)?\]\]' % escaped_title
+        pattern += r' *(?:\([^)]+\))?'
+        pattern += '(?:,| [-–]) *(.*)$'
+        return re.compile(pattern, re.M)
+
+    @staticmethod
+    def handle_link(m):
+        text = m.group(2)
+        if text:
+            return text.lstrip('|').strip()
+        else:
+            return m.group('title').strip()
+
+    def validate_description(self, desc):
+        return (bool(desc) and len(desc.split()) >= self.getOption('min_words'))
+
+    def parse_description(self, text):
+        desc = self.COMMENT_REGEX.sub('', text)
+        desc = NESTED_TEMPLATE_REGEX.sub('', desc)
+        desc = self.FILE_LINK_REGEX.sub('', desc)
+        desc = LINK_REGEX.sub(self.handle_link, desc)
+        desc = self.FORMATTING_REGEX.sub('', desc).replace('&nbsp;', ' ')
+        desc = self.REF_REGEX.sub('', desc.strip())
+        desc = re.sub(r' *\([^)]+\)$', '', desc.rstrip())
+        desc = desc.partition(';')[0]
+        desc = re.sub(r'^.*\) [-–] +', '', desc)
+        desc = re.sub(r'^\([^)]+\) +', '', desc)
+        while ' ' * 2 in desc:
+            desc = desc.replace(' ' * 2, ' ')
+        if re.search('[^IVX]\.$', desc) or desc.endswith(tuple(',:')):
+            desc = desc[:-1].rstrip()
+        if desc.startswith(('a ', 'an ')):
+            desc = desc.partition(' ')[2]
+        return desc
+
+    def get_pages_with_descriptions(self, text):
+        data = {}
+        for match in self.regex.finditer(text):
+            title, desc = match.groups()
+            page = pywikibot.Page(self.site, title)
+            data[page] = self.parse_description(desc)
+        return data
+
+    def treat_page(self):
+        page = self.current_page
+        descriptions = self.get_pages_with_descriptions(page.text)
+        add = False
+        for item in PreloadingEntityGenerator(descriptions.keys()):
+            if self.site.lang in item.descriptions:
+                continue
+            target = pywikibot.Page(self.site, item.getSitelink(self.site))
+            desc = descriptions.get(target)
+            if self.validate_description(desc):
+                with self.db.cursor() as cur:
+                    cur.execute('''
+INSERT INTO descriptions (item, lang, random, description)
+VALUES (%s, %s, %s, %s);''', (item.id, self.site.lang, random.randrange(2**32), desc))
+                    add = True
+        if add:
+            self.db.commit()
+
+
+def main(*args):
+    options = {}
+    local_args = pywikibot.handle_args(args)
+    site = pywikibot.Site()
+    genFactory = GeneratorFactory(site=site)
+    for arg in local_args:
+        if genFactory.handleArg(arg):
+            continue
+        if arg.startswith('-'):
+            arg, sep, value = arg.partition(':')
+            if value != '':
+                options[arg[1:]] = int(value) if value.isdigit() else value
+            else:
+                options[arg[1:]] = True
+
+    db = pymysql.connect(
+        database='s53805__data',
+        host='tools.db.svc.eqiad.wmflabs',
+        read_default_file=os.path.expanduser('~/replica.my.cnf'),
+        charset='utf8mb4',
+    )
+    generator = genFactory.getCombinedGenerator(preload=True)
+    bot = DescriptionsBot(db, generator=generator, site=site, **options)
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
